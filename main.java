7f6bf25e2cf0c26ac677a57275189cb2e5baf792
MansoorAlfalasi <mansoormubarak50@gmail.com>
2026-02-06 16:55:31 +0400

    Update main.java

--- main.java
+++ main.java
@@ -1,9 +1,10 @@
 public class main {
 
-    //This class is needed to run the program wriiten in the StudentManagerFrame.
+    //This class is needed to run the program wriiten in the LoginFrame.
     
     public static void main(String[] args) {
         new LoginFrame();
     }
 }
+
 
